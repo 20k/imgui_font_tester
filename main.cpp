32a5fd7c907e73fbc9f35f97d069b187547fbb63
James <Icedshot@gmail.com>
2019-04-17 14:17:21 +0100

    More testing, update deps

--- main.cpp
+++ main.cpp
@@ -46,7 +46,8 @@ int main()
 
     sf::Clock imgui_delta;
 
-    //ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(255, 255, 255, 255));
+    //ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(0, 0, 0, 255));
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(255, 255, 255, 255));
 
     while(window.isOpen())
     {
@@ -59,7 +60,7 @@ int main()
                 //io.Fonts->AddFontDefault();
 
                 if(!use_stb)
-                    io.Fonts->AddFontFromFileTTF("Arial.ttf", 14.f);
+                    io.Fonts->AddFontFromFileTTF("Arial.ttf", 18.f);
                 else
                     io.Fonts->AddFontDefault();
 
@@ -84,7 +85,7 @@ int main()
                 {
                     ImFontAtlas* atlas = ImGui::SFML::GetFontAtlas();
 
-                    uint32_t font_mode = ImGuiFreeType::ForceAutoHint;
+                    uint32_t font_mode = ImGuiFreeType::LCD;
 
                     ImGuiFreeType::BuildFontAtlas(atlas, font_mode, subpixel_mode);
 
@@ -119,11 +120,11 @@ int main()
 
         ImGui::Begin("Font Tester");
 
-        //ImGui::TextColored(ImVec4(1,0,0,1), "Hello font 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        //ImGui::TextColored(ImVec4(0,0,0,1), "The quick brown fox jumps over the lazy dog ({}).!<>/|\\");
+        ImGui::TextColored(ImVec4(0,0,0,1), "Hello font 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        ImGui::TextColored(ImVec4(0,0,0,1), "The quick brown fox jumps over the lazy dog ({}).!<>/|\\");
 
-        ImGui::Text("Hello font 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        ImGui::Text("The quick brown fox jumps over the lazy dog ({}).!<>/|\\");
+        ImGui::TextColored(ImVec4(1,1,1,1), "Hello font 1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        //ImGui::Text("The quick brown fox jumps over the lazy dog ({}).!<>/|\\");
 
         rebuild_font |= CheckBoxHelper(subpixel_mode, ImGuiFreeType::DEFAULT, "DEFAULT");
         rebuild_font |= CheckBoxHelper(subpixel_mode, ImGuiFreeType::LEGACY, "LEGACY");
@@ -138,7 +139,11 @@ int main()
 
         ImGui::End();
 
+        sf::Clock clk;
+
         ImGui::SFML::Render(window);
+
+        std::cout << "rclock " << clk.getElapsedTime().asMicroseconds() / 1000. << std::endl;
 
         window.display();
         window.clear();
